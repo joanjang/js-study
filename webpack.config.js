3bd22cca8ac08cbedddea23d2f1091456f721fdd
je0489 <je0489@naver.com>
2022-01-27 23:38:37 +0900

    Chore: Initial project ✨

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({ template: "index.html" }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+};
